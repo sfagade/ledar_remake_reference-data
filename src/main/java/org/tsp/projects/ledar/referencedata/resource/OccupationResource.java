f86f4dada1595c9e7f0ab689205799ed9e87579e
samsonfagade <sfagade@gmail.com>
2020-06-07 08:29:46 +0100

    created endpoint to view occupation by ID changed properties files to always update model when there is a change

--- src/main/java/org/tsp/projects/ledar/referencedata/resource/OccupationResource.java
+++ src/main/java/org/tsp/projects/ledar/referencedata/resource/OccupationResource.java
@@ -1,14 +1,14 @@
 package org.tsp.projects.ledar.referencedata.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.tsp.projects.ledar.referencedata.model.Occupation;
 import org.tsp.projects.ledar.referencedata.repository.OccupationsRepository;
 
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/occupationResource")
@@ -29,5 +29,16 @@ public class OccupationResource {
     @RequestMapping(value = "/fetchAllOccupations", method = RequestMethod.GET)
     public List<Occupation> fetchAllOccupations() {
         return occupationRepository.findAll();
+    }
+
+    /**
+     * This method is used to fetch a single Occupation
+     *
+     * @return Occupations collection
+     */
+    @RequestMapping(value = "/viewOccupation/{occupationId}", method = RequestMethod.GET)
+    public Occupation viewOccupation(@PathVariable("occupationId") long occupationId) {
+        Optional<Occupation> optionalOccupation = occupationRepository.findById(occupationId);
+        return optionalOccupation.get();
     }
 }
